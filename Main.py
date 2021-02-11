b3330c35b5e45dae8126e1418c1f64e007cc047d
Surya Thakur <77577564+Surya0705@users.noreply.github.com>
2021-02-11 21:21:30 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,5 @@
+a = int(input("Enter the Number: ")) # Taking the Input from the User.
+b = a ** (1.0/3.0) # Finding its Cube Root. Remember that two '**' mean raise to the Power.
+c = round(b) # This line Rounds off the Output to Provide us with the Correct Answer.
+
+print(f"The Cube Root of {a} is {c}.") # Printing the Results.
